905b00011b3559646a8b7a3c1272dfd4bd9e497d
wuyuxin <876465382@qq.com>
2021-02-19 18:01:15 +0800

    finnish unit-1 player control

---
+++ Assets/Unit1-PlayerControl/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MaxVelocity=20f;
+    public float MaxTurnVelocity=5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * MaxVelocity * Time.deltaTime*forwardInput);
+        transform.Rotate(Vector3.up * MaxTurnVelocity * Time.deltaTime*horizontalInput*forwardInput);
+    }
+}
