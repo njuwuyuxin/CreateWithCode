7dfd742549257f2af25685dfe3235037056427fd
wuyuxin <876465382@qq.com>
2021-03-18 21:43:06 +0800

    finish next steps

--- Assets/Unit-1 PlayerControl/Scripts/FollowPlayer.cs
+++ Assets/Unit-1 PlayerControl/Scripts/FollowPlayer.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset = new Vector3(0, 5, -7);
+    //private Vector3 offset = new Vector3(0, 5, -7);
+    [SerializeField] private Vector3 offset = new Vector3(0, 2, 0.3f);
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void LateUpdate()
--- Assets/Unit-1 PlayerControl/Scripts/PlayerController.cs
+++ Assets/Unit-1 PlayerControl/Scripts/PlayerController.cs
@@ -1,23 +1,65 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    public float MaxVelocity=20f;
-    public float MaxTurnVelocity=5f;
+    [SerializeField] float horsePower=20000f;
+    [SerializeField] float maxTurnVelocity=40f;
+    private float speed;
+    private float rpm;
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] int wheelsOnGround;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
-
     // Update is called once per frame
     void Update()
     {
+        speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f);
+        speedometerText.text = "Speed: " + speed + " mph";
+        rpm = Mathf.Round((speed % 30) * 40);
+        rpmText.text = "Rpm: " + rpm;
+    }
+
+    void FixedUpdate()
+    {
         float forwardInput = Input.GetAxis("Vertical");
         float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.forward * MaxVelocity * Time.deltaTime*forwardInput);
-        transform.Rotate(Vector3.up * MaxTurnVelocity * Time.deltaTime*horizontalInput*forwardInput);
+        if (IsOnGround())
+        {
+            playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
+            //transform.Translate(Vector3.forward * maxTurnVelocity * Time.deltaTime*forwardInput);
+            transform.Rotate(Vector3.up * maxTurnVelocity * Time.deltaTime * horizontalInput);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
